4c94ab184ffb7691c9cfb76165e7251c6736b4d1
Umme Kulsoom <107399532+ukulsoomm@users.noreply.github.com>
2024-08-22 10:23:43 +0530

    Update typewise-alert.cpp

--- typewise-alert.cpp
+++ typewise-alert.cpp
@@ -2,13 +2,7 @@
 #include <stdio.h>
 
 BreachType inferBreach(double value, double lowerLimit, double upperLimit) {
-  if(value < lowerLimit) {
-    return TOO_LOW;
-  }
-  if(value > upperLimit) {
-    return TOO_HIGH;
-  }
-  return NORMAL;
+    return (value < lowerLimit) ? TOO_LOW : (value > upperLimit) ? TOO_HIGH : NORMAL;
 }
 
 Limits getLimits(CoolingType coolingType) {
